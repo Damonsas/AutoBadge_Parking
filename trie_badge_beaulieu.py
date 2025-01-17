802948e4a736265041ee5a922a618b6d65753e75
Damonsas <danh.kh.pro@gmail.com>
2025-01-17 19:04:31 +0100

    Update trie_badge_beaulieu.py

--- trie_badge_beaulieu.py
+++ trie_badge_beaulieu.py
@@ -1,11 +1,17 @@
 from turtle import pd
 import pandas
-df = pandas.read_excel('C:/Users/danhk/OneDrive/Bureau/Liste_Abonnés_test.xlsx') 
-
+df = pandas.read_excel('C:/Users/danhk/OneDrive/Bureau/Liste_Abonnés.xlsx') 
+n = ''
 # print (df.info)
 
 # print (df.values)
-i = 'non affecté'
 
 for i in df.values :
-    print (i [2])
+    # print (i [2])
+    if 'Non Affecté' == i [2]:
+       print (i [0], i [2]) 
+    
+
+for i in df.values :
+    if n == i [0]:
+        print (i[0], i [2])
