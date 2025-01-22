09dcb8c5a378882645abce54f7594eab9a72855b
Damonsas <danh.kh.pro@gmail.com>
2025-01-22 19:11:13 +0100

    Update trie_badge_beaulieu.py

--- trie_badge_beaulieu.py
+++ trie_badge_beaulieu.py
@@ -1,16 +1,23 @@
+import sys
 from turtle import pd
 import pandas
-df = pandas.read_excel('C:/Users/danhk/OneDrive/Bureau/Liste_Abonnés.xlsx') 
-n = 3
-# print (df.info)
 
-# print (df.values)
+df = pandas.read_excel('C:/Users/danhk/OneDrive/Bureau/Liste_Abonnés.xlsx') 
+# n = 7575
 
 # for i in df.values :
-    # print (i [2])
-    # if 'Non Affecté' == i [2]:
-    #    print (i [0], i [2]) 
+    # if n == i [0]:
+        # print (i[0], i [2], i [3], i [5])
 
-for i in df.values :
-    if n == i [0]:
-        print (i[0], i [2], i [3], i [5])
+
+print ("trie_badge_beaulieu:", sys.argv[1])
+
+for i in df.values : 
+    if int(sys.argv[1]) == i [0]:
+
+        print (i[0], i[3], i[5])
+
+    # elif int(sys.argv[1]) != i [0]:
+        # print("not used") 
+    #  else :
+        # print ("(sys.argv[1]) is not a number")
