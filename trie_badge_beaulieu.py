70dac2f021bdcbcc9d1b679719cc716c311281e7
Damonsas <danh.kh.pro@gmail.com>
2025-01-23 17:17:40 +0100

    Update trie_badge_beaulieu.py

--- trie_badge_beaulieu.py
+++ trie_badge_beaulieu.py
@@ -9,14 +9,15 @@ df = pandas.read_excel('C:/Users/danhk/OneDrive/Bureau/Liste_Abonnés.xlsx')
     # if n == i [0]:
         # print (i[0], i [2], i [3], i [5])
 
-
 print ("trie_badge_beaulieu:", sys.argv[1])
 
+found = False   
 for i in df.values : 
     if int(sys.argv[1]) == i [0]:
-         print (i[0], i[3], i[5])
-else :
-    if int(sys.argv[1]) != i[0]:
-        print ("not used") 
-  #  else :
-        # print ("(sys.argv[1]) is not a number")
+        found = True
+        print (i[0], i[3], i[5])
+         
+
+if not found :
+    print ("not used")
+
